c1fcdff4b86039f70cae1c2f11f3301d0ffd9c26
starodubov <s.s.starodubov@yandex.ru>
2025-05-09 20:44:01 +0400

    http server

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ mod server;
 use std::io::Write;
 use std::thread;
 use std::time::Duration;
+use reqwest::blocking::Client;
 use crate::server::HttpServerConfig;
 use server::HttpServer;
 
 fn main() {
     HttpServer::start_on_thread(HttpServerConfig::new().port(8080));
     thread::sleep(Duration::from_secs(1));
-    let response = isahc::get("http://localhost:8080");
+    let response = Client::new().get("http://localhost:8080/").send();
     // Выводим статус и тело
     println!("response: {:?}", response);
 }
